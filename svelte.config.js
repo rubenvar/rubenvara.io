740847653d2ac42313f269dcaa2d59d2b02e87c6
Rubén <hey@rubenvara.io>
2022-01-25 14:16:15 +0100

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
+import netlify from '@sveltejs/adapter-netlify';
 import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,6 +15,7 @@ const config = {
     mdsvex(mdsvexConfig),
   ],
   kit: {
+    adapter: netlify(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: {
