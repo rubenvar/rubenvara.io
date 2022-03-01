8db0c80d29210f98eb6dfed2763b0480100a21b6
Rubén <hey@rubenvara.io>
2022-03-01 16:43:39 +0100

    add draft text in listed post in dev

--- src/lib/ListedPost.svelte
+++ src/lib/ListedPost.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { dev } from "$app/env";
+
   import PostMeta from "./PostMeta.svelte";
 
   export let post;
@@ -10,6 +12,9 @@
   </h2>
   {#if post.description}
     <p class="description">{post.description}</p>
+  {/if}
+  {#if dev && post.status === "draft"}
+    <p class="description">(draft)</p>
   {/if}
   <PostMeta date={post.updated || post.date} categories={post.categories} />
 </article>
