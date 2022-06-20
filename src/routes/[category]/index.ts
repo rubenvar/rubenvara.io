da60251635a8358c9632adc43dc1e28c83e82149
Rubén <hey@rubenvara.io>
2022-06-20 23:55:45 +0200

    fix

--- src/routes/[category]/index.ts
+++ src/routes/[category]/index.ts
@@ -1,16 +1,14 @@
 import { dev } from '$app/env';
 import type { RequestHandler } from './__types';
 import { getAllPosts } from '../_api';
-import {categories} from '$lib/assets/categories';
 
 export const get: RequestHandler = async ({ params }) => {
   const { category } = params;
   const posts = await getAllPosts(dev, category);
-  const description = categories.find(cat => cat.name === category)?.description;
 
   if (posts.length) {
     return {
-      body: { posts, description },
+      body: { posts },
     };
   }
 
