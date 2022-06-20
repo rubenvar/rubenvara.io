6d1ad776c0d345af28ada601b27948bf4ed97d3a
Rubén <hey@rubenvara.io>
2022-06-20 21:57:25 +0200

    show title in category page

--- src/routes/[category]/index.svelte
+++ src/routes/[category]/index.svelte
@@ -8,9 +8,28 @@
   const { category } = $page.params;
 </script>
 
-<h1>The <strong>{category}</strong> category page</h1>
-<p>Here will list all posts in this category</p>
+<header>
+  <h1>Todos los artículos en <span>`{category}`</span></h1>
+</header>
 
 {#each posts as post}
   <ListedPost {post} />
 {/each}
+
+<style lang="scss">
+  header {
+    margin: 0 0 var(--gap100);
+    @media only screen and (min-width: 480) {
+      margin-bottom: var(--gap100);
+    }
+  }
+  h1 {
+    font-size: var(--fz80);
+    span {
+      display: inline-block;
+      background: var(--grey100);
+      border-radius: 6px;
+      padding: 2px var(--gap10);
+    }
+  }
+</style>
