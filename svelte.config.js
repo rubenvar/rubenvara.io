51cfe03fee025c7b8b5f7fa6d175419cbbaf278d
Rubén <hey@rubenvara.io>
2022-07-11 12:30:14 +0200

    update SvelteKit, migrate to new Vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,8 @@
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
 import { mdsvex } from 'mdsvex';
 import preprocess from 'svelte-preprocess';
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
 import mdsvexConfig from './mdsvex.config.js';
-
-const file = fileURLToPath(new URL('package.json', import.meta.url));
-const json = readFileSync(file, 'utf8');
-const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,33 +11,6 @@ const config = {
     trailingSlash: 'always',
     prerender: {
       default: true,
-    },
-    vite: {
-      // https://github.com/vitejs/vite/issues/6333
-      postcss: {
-        plugins: [
-          {
-            postcssPlugin: 'internal:charset-removal',
-            AtRule: {
-              charset: (atRule) => {
-                if (atRule.name === 'charset') {
-                  atRule.remove();
-                }
-              },
-            },
-          },
-        ],
-      },
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: '@use "src/variables.scss";',
-          },
-        },
-      },
-      define: {
-        __PKG_VERSION__: JSON.stringify(pkg.version),
-      },
     },
   },
   extensions: ['.svelte', ...mdsvexConfig.extensions],
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  // https://github.com/vitejs/vite/issues/6333
+  postcss: {
+    plugins: [
+      {
+        postcssPlugin: 'internal:charset-removal',
+        AtRule: {
+          charset: (atRule) => {
+            if (atRule.name === 'charset') {
+              atRule.remove();
+            }
+          },
+        },
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "src/variables.scss";',
+      },
+    },
+  },
+  define: {
+    __PKG_VERSION__: JSON.stringify(pkg.version),
+  },
+  plugins: [sveltekit()],
+};
+
+export default config;
