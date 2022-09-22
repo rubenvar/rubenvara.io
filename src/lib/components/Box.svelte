46093a52f1f1e4f6bf2d444d2c66265d14cdef44
Rubén <hey@rubenvara.io>
2022-09-22 15:35:40 +0200

    fix box component css

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,6 +1,6 @@
 <!-- unused -->
 <script lang="ts">
-  export let type: 'info';
+  export let type = 'info';
 </script>
 
 <div class="box {type}">
@@ -12,11 +12,23 @@
     border: 1px solid;
     padding: var(--gap40);
     padding-bottom: 0; /* ಠ_ಠ */
+    border-radius: var(--radius10);
+    margin-bottom: var(--gap60);
     > *:last-child {
       margin-bottom: 0;
     }
     &.info {
-      color: green;
+      border-color: var(--note500);
+    }
+    &.updated {
+      border-color: orange;
+    }
+    &.recuerda {
+      border-color: var(--announce500);
+    }
+    &.traduccion {
+      border-width: 2px;
+      border-color: var(--grey600);
     }
   }
 </style>
