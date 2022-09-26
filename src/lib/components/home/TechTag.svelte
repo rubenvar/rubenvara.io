42bee81ec0d83ee07162d1269a9e9d0ce4c888b4
Rubén <hey@rubenvara.io>
2022-09-26 13:08:30 +0200

    tech tags

--- src/lib/components/home/TechTag.svelte
+++ src/lib/components/home/TechTag.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   export let tech: string;
-  
-  const cl = tech
+
+  const techName = tech
     .toLowerCase()
     .replace('.', '')
     .replace(/\s\d+/, '')
     .replace(' ', '-');
 </script>
 
-<span class="tag"><span class="dot {cl}" />{tech}</span>
+<span class="tag">
+  <span class="dot" style="background-color: var(--{techName});" />
+  {tech}
+</span>
 
 <style lang="scss">
   .tag {
@@ -28,79 +31,5 @@
     height: 8px;
     border-radius: 50%;
     background-color: black;
-    &.svelte,
-    &.sapper,
-    &.sveltekit {
-      background-color: var(--svelte);
-    }
-    &.netlify {
-      background-color: var(--netlify);
-    }
-    &.vanillajs {
-      background-color: var(--vanillajs);
-    }
-    &.vanillajs {
-      background-color: var(--vanillajs);
-    }
-    &.scss {
-      background-color: var(--scss);
-    }
-    &.pug {
-      background-color: var(--pug);
-    }
-    &.parcel {
-      background-color: var(--parcel);
-    }
-    &.express {
-      background-color: var(--express);
-    }
-    &.mongoose {
-      background-color: var(--mongoose);
-    }
-    &.webpack {
-      background-color: var(--webpack);
-    }
-    &.aws-lambda {
-      background-color: var(--aws-lambda);
-    }
-    &.digitalocean {
-      background-color: var(--digitalocean);
-    }
-    &.react {
-      background-color: var(--react);
-    }
-    &.nextjs {
-      background-color: var(--nextjs);
-    }
-    &.typescript {
-      background-color: var(--typescript);
-    }
-    &.graphql {
-      background-color: var(--graphql);
-    }
-    &.react-query {
-      background-color: var(--react-query);
-    }
-    &.graphql-codegen {
-      background-color: var(--graphql-codegen);
-    }
-    &.keystonejs {
-      background-color: var(--keystonejs);
-    }
-    &.nodejs {
-      background-color: var(--nodejs);
-    }
-    &.mongodb {
-      background-color: var(--mongodb);
-    }
-    &.prisma {
-      background-color: var(--prisma);
-    }
-    &.wordpress {
-      background-color: var(--wordpress);
-    }
-    &.php {
-      background-color: var(--php);
-    }
   }
 </style>
