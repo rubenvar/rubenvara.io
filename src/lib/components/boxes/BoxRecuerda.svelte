b07a3bd6ae3464fa83b391a9004155265336ebbb
Rubén <hey@rubenvara.io>
2022-10-17 12:13:18 +0200

    style recuerda box

--- src/lib/components/boxes/BoxRecuerda.svelte
+++ src/lib/components/boxes/BoxRecuerda.svelte
@@ -1,52 +1,38 @@
-<script lang="ts">
-  // let size = 1;
-
-  function handleClick() {
-    console.log('what');
-
-    // size = size + 1;
-  }
-</script>
-
 <div class="box recuerda">
-  <button on:click={handleClick}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-radioactive"
-      width="40"
-      height="40"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M13.5 14.6l3 5.19a9 9 0 0 0 4.5 -7.79h-6a3 3 0 0 1 -1.5 2.6" />
-      <path d="M13.5 9.4l3 -5.19a9 9 0 0 0 -9 0l3 5.19a3 3 0 0 1 3 0" />
-      <path d="M10.5 14.6l-3 5.19a9 9 0 0 1 -4.5 -7.79h6a3 3 0 0 0 1.5 2.6" />
-    </svg>
-  </button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <circle cx="12" cy="12" r="2" />
+    <path
+      d="M11.939 14c0 .173 .048 .351 .056 .533l0 .217a4.75 4.75 0 0 1 -4.533 4.745l-.217 0m-4.75 -4.75a4.75 4.75 0 0 1 7.737 -3.693m6.513 8.443a4.75 4.75 0 0 1 -4.69 -5.503l-.06 0m1.764 -2.944a4.75 4.75 0 0 1 7.731 3.477l0 .217m-11.195 -3.813a4.75 4.75 0 0 1 -1.828 -7.624l.164 -.172m6.718 0a4.75 4.75 0 0 1 -1.665 7.798"
+    />
+  </svg>
   <slot />
 </div>
 
 <style lang="scss">
   .box {
     position: relative;
-    button {
-      position: absolute;
-      right: var(--gap60);
-      top: var(--gap60);
-    }
   }
   svg {
-    border: 1px solid;
     stroke: var(--announce600);
-    transition: transform 0.2s;
-    cursor: pointer;
-    /* &:hover {
+    transition: transform 0.35s;
+    width: var(--fz50);
+    height: var(--fz50);
+    position: absolute;
+    right: var(--gap20);
+    top: var(--gap20);
+    &:hover {
       transform: scale(0.95);
-    } */
+    }
   }
 </style>
