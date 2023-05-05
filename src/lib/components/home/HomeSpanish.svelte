8c70b86a10f4f8650efb0436f859174993d4e9c8
Rubén <hey@rubenvara.io>
2023-05-05 12:42:56 +0200

    tweak home texts

--- src/lib/components/home/HomeSpanish.svelte
+++ src/lib/components/home/HomeSpanish.svelte
@@ -7,6 +7,7 @@
   import CardVpnf from './CardVpnf.svelte';
   import { dev } from '$app/environment';
   import ReadMore from './ReadMore.svelte';
+  import SocialLinks from './SocialLinks.svelte';
 
   const things = [
     'nado',
@@ -27,10 +28,10 @@
   proyectos web.
 </p>
 <img src={rvImage} alt="Rubén Vara" width={300} height={300} />
-<p>
-  Durante la década pasada estudié ingeniería, abandoné una vida para empezar
-  otra viviendo por el mundo y creando mi negocio online, y casi superé una
-  depresión.
+<p class="about-me">
+  Durante la década pasada estudié ingeniería, cometí muchos errores, y abandoné
+  una vida para empezar otra viviendo por el mundo y creando mi negocio online.
+  Como en todo gran viaje, al final volví al inicio.
 </p>
 <ReadMore href="/about">Lee más sobre mí ⟶</ReadMore>
 
@@ -38,23 +39,27 @@
   <div class="project-intro">
     <h2 class="section-title">Algunos proyectos</h2>
     <p>
-      Desde hace unos cuantos años, todas las semanas tengo ideas que se
-      convierten en proyectos. Algunos los termino. A veces funcionan. Siempre
-      aprendo algo nuevo, todos los días.
+      Desde hace unos cuantos años, todas las semanas tengo ideas que convierto
+      en proyectos. Algunos los termino. A veces funcionan. Siempre aprendo algo
+      nuevo, todos los días.
     </p>
   </div>
   <CardCaa />
   <CardTn />
   <CardVpnf />
   <p class="project-outro">
-    Además, <a href="https://rubenvar.github.io/" target="_blank">aquí</a>
-    puedes ver otros proyectos que he creado, y en mi
+    Además, <a href="https://rubenvar.github.io/" target="_blank"
+      >un listado con más proyectos</a
+    >.
+  </p>
+  <p>
+    En mi
     <a href="https://github.com/rubenvar" target="_blank">GitHub</a>
     está todo el código que escribo a diario, además de unos cuantos
     <em>npm packages</em> que he publicado.
   </p>
 {/if}
-
+<SocialLinks />
 <h2 class="section-title">Posts más recientes</h2>
 <LatestPosts />
 
@@ -79,6 +84,10 @@
     border-radius: 50%;
     margin-bottom: var(--gap60);
   }
+  .about-me {
+    margin-bottom: var(--gap40);
+  }
+
   .project-intro {
     margin-bottom: var(--gap80);
     p {
@@ -86,6 +95,7 @@
     }
   }
   .project-outro {
+    color: var(--grey700);
     margin-top: var(--gap80);
   }
 </style>
