203920e8f336bb06251f764617d6e8eca5e3bd38
Rubén <hey@rubenvara.io>
2023-05-05 14:02:00 +0200

    add images in cards

--- src/lib/components/home/ProjectCard.svelte
+++ src/lib/components/home/ProjectCard.svelte
@@ -5,6 +5,8 @@
   export let id: string;
   export let title: string;
   export let techs: string[] = [];
+  export let browser: string;
+  export let iphone: string;
 
   // --caa600
   // --tn500
@@ -30,14 +32,30 @@
       </div>
     </div>
   </button>
+  <img class="screen" src={browser} alt="Página Principal de {title}" />
+  <img class="iphone" src={iphone} alt="{title} en iPhone" />
 </a>
 
 <style lang="scss">
   a {
     margin: var(--gap40) 0 var(--gap90);
     display: block;
+    position: relative;
     &:last-of-type {
       margin-bottom: var(--gap60);
+    }
+    &:hover {
+      button::before {
+        opacity: 1;
+        transition: opacity 0.4s;
+      }
+      img {
+        scale: 1.75;
+        translate: calc(60% + 15px) 90px;
+        &.iphone {
+          translate: calc(100% + 95px) 90px;
+        }
+      }
     }
   }
   button {
@@ -47,12 +65,34 @@
     background: linear-gradient(
       -210deg,
       var(--customColor),
-      hsla(0, 0%, 99%, 0.9) 55%
+      hsla(0, 0%, 99%, 0.9) 55% /* --white variable with alpha, hardcoded */
     );
     box-shadow: none;
     border: none;
     text-align: left;
     font-size: var(--fz30);
+    position: relative;
+    z-index: 1;
+    &::before {
+      /* background gradient transition trick */
+      border-radius: var(--radius30);
+      background: var(--black);
+      background: linear-gradient(
+        -210deg,
+        var(--customColor),
+        var(--grey700) 55%,
+        var(--customColor)
+      );
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      opacity: 0;
+      transition: opacity 0.75s;
+    }
     .inner {
       background-color: var(--white);
       border-radius: var(--radius20);
@@ -67,6 +107,27 @@
         gap: var(--gap20);
         margin-bottom: var(--gap10);
       }
+    }
+  }
+  img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 1;
+    &.screen {
+      border-radius: var(--radius20);
+      box-shadow: var(--bs20);
+      translate: calc(90% + 15px) 70px;
+      rotate: 3deg;
+      max-width: 250px;
+      transition: scale 0.3s, translate 0.3s;
+    }
+    &.iphone {
+      filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.15));
+      translate: calc(100% + 50px) 160px;
+      rotate: -4deg;
+      max-height: 140px;
+      transition: scale 0.3s, translate 0.3s;
     }
   }
 </style>
