0dcca29725e988943c2aa47027002c4b443498af
Rubén <hey@rubenvara.io>
2023-05-08 00:16:27 +0200

    mobile styling

--- src/lib/components/home/ProjectCard.svelte
+++ src/lib/components/home/ProjectCard.svelte
@@ -12,6 +12,9 @@
   // --tn500
   // --vpnf600
   const customColor = `${id}600`;
+
+  // imgs outside button only show if widerthan 1200px
+  // if not, images inside .inner will show
 </script>
 
 <a href="https://{title}" target="_blank">
@@ -22,6 +25,18 @@
   >
     <div class="inner">
       <h3>{title}</h3>
+      <div class="mobile-images">
+        <img
+          class="mobile-screenshot mobile-browser"
+          src={browser}
+          alt="página pricipal de {title}"
+        />
+        <img
+          class="mobile-screenshot mobile-iphone"
+          src={iphone}
+          alt="{title} en iPhone"
+        />
+      </div>
       <div class="text">
         <slot />
       </div>
@@ -32,8 +47,12 @@
       </div>
     </div>
   </button>
-  <img class="screen" src={browser} alt="Página Principal de {title}" />
-  <img class="iphone" src={iphone} alt="{title} en iPhone" />
+  <img
+    class="screenshot browser"
+    src={browser}
+    alt="Página Principal de {title}"
+  />
+  <img class="screenshot iphone" src={iphone} alt="{title} en iPhone" />
 </a>
 
 <style lang="scss">
@@ -45,15 +64,26 @@
       margin-bottom: var(--gap60);
     }
     &:hover {
-      button::before {
-        opacity: 1;
-        transition: opacity 0.4s;
-      }
-      img {
-        scale: 1.35;
-        translate: calc(30% + 15px) 90px;
-        &.iphone {
-          translate: calc(100%) 90px;
+      @media only screen and (min-width: 768px) {
+        button::before {
+          opacity: 1;
+          transition: opacity 0.4s;
+        }
+        .screenshot {
+          scale: 1.35;
+          translate: calc(30% + 15px) 90px;
+          &.iphone {
+            translate: calc(100%) 90px;
+          }
+        }
+        .mobile-screenshot {
+          scale: 1.35;
+          &.mobile-browser {
+            translate: 50px 30px;
+          }
+          &.mobile-iphone {
+            translate: calc(100%) 90px;
+          }
         }
       }
     }
@@ -73,6 +103,13 @@
     font-size: var(--fz30);
     position: relative;
     z-index: 1;
+    @media only screen and (max-width: 768px) {
+      background: linear-gradient(
+        -200deg,
+        var(--customColor),
+        hsla(0, 0%, 99%, 0.9) 85% /* --white variable with alpha, hardcoded */
+      );
+    }
     &::before {
       /* background gradient transition trick */
       border-radius: var(--radius30);
@@ -101,6 +138,37 @@
         margin-top: 0;
         margin-bottom: var(--gap40);
       }
+      .mobile-images {
+        width: 75%;
+        margin: 0 0 var(--gap40) var(--gap40);
+        position: relative;
+        @media only screen and (min-width: 1200px) {
+          display: none;
+        }
+        @media only screen and (min-width: 768px) {
+          width: 50%;
+        }
+        @media only screen and (min-width: 576px) {
+          width: 65%;
+        }
+        img {
+          display: block;
+          transition: scale 0.3s, translate 0.3s;
+        }
+        .mobile-browser {
+          rotate: -1deg;
+          box-shadow: var(--bs20);
+        }
+        .mobile-iphone {
+          position: absolute;
+          top: 0;
+          right: 0;
+          max-height: 70%;
+          translate: -50px 20px;
+          filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.15));
+          rotate: 4deg;
+        }
+      }
       .tech {
         display: flex;
         flex-wrap: wrap;
@@ -109,25 +177,33 @@
       }
     }
   }
-  img {
+  .screenshot {
     position: absolute;
     top: 0;
     right: 0;
     z-index: 1;
-    &.screen {
+    transition: scale 0.3s, translate 0.3s;
+    &.browser {
       border-radius: var(--radius20);
       box-shadow: var(--bs20);
       translate: calc(90% + 15px) 70px;
       rotate: 3deg;
       max-width: 250px;
-      transition: scale 0.3s, translate 0.3s;
     }
     &.iphone {
       filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.15));
       translate: calc(100% + 50px) 160px;
       rotate: -4deg;
       max-height: 140px;
-      transition: scale 0.3s, translate 0.3s;
+    }
+    @media only screen and (max-width: 1200px) {
+      display: none;
+      position: relative;
+      &.browser,
+      &.iphone {
+        translate: none;
+        rotate: none;
+      }
     }
   }
 </style>
