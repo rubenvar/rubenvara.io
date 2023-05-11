6deebe25d48fee5fcb4c761b1968b8ad93282817
Rubén <hey@rubenvara.io>
2023-05-11 15:21:52 +0200

    cleanup

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,11 +3,13 @@ import type { LayoutLoad } from './$types';
 export const prerender = true;
 export const trailingSlash = 'always';
 
-export const load: LayoutLoad = ({ url }) => ({
-  key: url.pathname,
-  canonical: `${url.origin}${url.pathname}`,
-  title:
-    'Rubén Vara ~ Mi Blog sobre Javascript, Desarrollo Web, y Otras Historias',
-  description:
-    'Web Personal: Qué hago ahora, mi blog sobre desarrollo web y Javascript, mi estilo de vida, y mi primer gran viaje. Un poco de todo',
-});
+export const load = (({ url }) => {
+  return {
+    key: url.pathname,
+    canonical: `${url.origin}${url.pathname}`,
+    title:
+      'Rubén Vara ~ Mi Blog sobre Javascript, Desarrollo Web, y Otras Historias',
+    description:
+      'Web Personal: Qué hago ahora, mi blog sobre desarrollo web y Javascript, mi estilo de vida, y mi primer gran viaje. Un poco de todo',
+  };
+}) satisfies LayoutLoad;
